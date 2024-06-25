e3373e5280a61d0b3755528b65d2e6b4431cfe17
joaol405 <joaolucasaraujomoura92@gmail.com>
2024-06-25 18:47:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+//Português
+$(function() {
+    $(".br").click(function() {
+        //cabeçalho
+        $(".nav-menu").children().eq(0).text("Html");
+        $(".nav-menu").children().eq(1).text("Css");
+        $(".nav-menu").children().eq(2).text("JavaScripty");
+        $(".nav-menu").children().eq(3).text("Java");
+        $(".language-selected").text("pt-BR");
+        $(".language-selected").removeClass("change-en");
+        $(".language-selected").removeClass("change-es");
+        $(".language-selected").addClass("change-br");
+        //seção
+        $("#title").text("APRENDA A PROGRAMAR");
+        $("#title2").text("com o melhor site desemvolvedor web do mercado ");
+       
+        
+
+
+        //sobre
+        $(".about-container ul:nth-child(1) li:nth-child(1) a").text("Solução");
+        $(".about-container ul:nth-child(1) li:nth-child(2) a").text("Visão Geral");
+        $(".about-container ul:nth-child(1) li:nth-child(3) a").text("Pagamentos");
+        $(".about-container ul:nth-child(2) li:nth-child(1) a").text("Suporte");
+        $(".about-container ul:nth-child(2) li:nth-child(2) a").text("Central de Ajuda");
+        $(".about-container ul:nth-child(2) li:nth-child(3) a").text("Termos e Políticas");
+    });
+});
+
+//English
+$(function() {
+    $(".en").click(function() {
+        //header
+        $(".nav-menu").children().eq(0).text("Html");
+        $(".nav-menu").children().eq(1).text("Css");
+        $(".nav-menu").children().eq(2).text("JavaScripty");
+        $(".nav-menu").children().eq(3).text("Java");
+        $(".language-selected").text("en-US");
+        $(".language-selected").removeClass("change-es");
+        $(".language-selected").removeClass("change-br");
+        $(".language-selected").addClass("change-en");
+        //section
+        $("#title").text("LEARN TO PROGRAM");
+        $("#title2").text("with the best web development site in the world");
+        //About
+        $(".about-container ul:nth-child(1) li:nth-child(1) a").text("Help Center");
+        $(".about-container ul:nth-child(1) li:nth-child(2) a").text("Ethics Line");
+        $(".about-container ul:nth-child(1) li:nth-child(3) a").text("Payments");
+        $(".about-container ul:nth-child(2) li:nth-child(1) a").text("Support");
+        $(".about-container ul:nth-child(2) li:nth-child(2) a").text("Help Center");
+        $(".about-container ul:nth-child(2) li:nth-child(3) a").text("Terms and Policies");
+    });
+});
+
+//Espanõl
+$(function() {
+    $(".es").click(function() {
+        //encabezamiento
+        $(".nav-menu").children().eq(0).text("Html");
+        $(".nav-menu").children().eq(1).text("Css");
+        $(".nav-menu").children().eq(2).text("JavaScripty");
+        $(".nav-menu").children().eq(3).text("Java");
+        $(".language-selected").text("es");
+        $(".language-selected").removeClass("change-br");
+        $(".language-selected").removeClass("change-en");
+        $(".language-selected").addClass("change-es");
+        //sección
+        $("#title").text("APRENDER A PROGRAMAR"); 
+        $("#title2").text("con el mejor sitio de desarrollo web del mundo");
+        
+        //sobre
+        $(".about-container ul:nth-child(1) li:nth-child(1) a").text("Soluciones");
+        $(".about-container ul:nth-child(1) li:nth-child(2) a").text("Visión General");
+        $(".about-container ul:nth-child(1) li:nth-child(3) a").text("Pagos");
+        $(".about-container ul:nth-child(2) li:nth-child(1) a").text("Soporte");
+        $(".about-container ul:nth-child(2) li:nth-child(2) a").text("Central de Ayuda");
+        $(".about-container ul:nth-child(2) li:nth-child(3) a").text("Términos y Políticas");
+    });
+});
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
+});
